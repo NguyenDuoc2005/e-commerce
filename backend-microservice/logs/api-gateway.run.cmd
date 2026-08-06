a737f9396305f432c539d2b5bb56c834503f8a10
duocnguyen1-tech <duoc.nguyen1@ntq-solution.com.vn>
2026-08-06 09:25:50 +0700

    chore: remove JVM crash logs and ignore them going forward

--- backend-microservice/logs/api-gateway.run.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\My Project\e-commerce"
-set "EUREKA_DEFAULT_ZONE=http://localhost:8761/eureka"
-call "C:\My Project\e-commerce\BE\gradlew.bat" -p "C:\My Project\e-commerce\backend-microservice" :api-gateway:bootRun > "C:\My Project\e-commerce\backend-microservice\logs\api-gateway.out.log" 2>&1
--- backend-microservice/logs/discovery-server.run.ps1
+++
@@ -1,3 +0,0 @@
-﻿Set-Location "C:\My Project\e-commerce"
-$env:EUREKA_DEFAULT_ZONE="http://localhost:8761/eureka"
-& "C:\My Project\e-commerce\BE\gradlew.bat" -p "C:\My Project\e-commerce\backend-microservice" :discovery-server:bootRun *> "C:\My Project\e-commerce\backend-microservice\logs\discovery-server.out.log"
